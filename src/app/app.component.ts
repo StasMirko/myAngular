9bf7b3cbd4b69b64bbbb309a6cf33aa377f8e1ea
Стас <stasmq@gmail.com>
2021-09-01 12:24:48 +0300

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myAngular';
+
+  title = 'Dynamic title';
+  number = 42;
+  arr =[1,2,3];
+
+  // img = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg';
+
+  inputValue = ''
+
+  constructor() {
+    // setTimeout(() => {
+    //   console.log('Timeout is over');
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/1/17/Angular_one_color_inverse_logo.svg';
+    // }, 5000)
+  }
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value
+  }
+
+  onBlur(str: string){
+    this.inputValue = str
+  }
+
+  onClick(){
+    console.log('Click')
+  }
+
 }
